375b35a8f7e489341f096aeff7ee5a8eb6203c61
Noel <noelmugisha332@gmail.com>
2023-06-27 19:37:01 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
+import CreateBlog from './CreateBlog';
+import BlogDetails from './BlogDetails';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+
+            <Route exact path='/'>
+              <Home />
+            </Route>
+
+            <Route path='/create'>
+              <CreateBlog />
+            </Route>
+
+            <Route path='/blogs/:id'>
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
